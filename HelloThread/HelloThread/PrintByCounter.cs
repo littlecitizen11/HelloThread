965cfaecedf92e49240322eea44103e6d7ec4b6a
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-08-24 21:44:47 +0300

    add project

---
+++ HelloThread/HelloThread/PrintByCounter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloThread
+{
+    public class PrintByCounter
+    {
+        private int Counter { get; set; }
+        private int CounterMamas { get; set; }
+        private int CounterEmpire { get; set; }
+        private object _lock;
+        public PrintByCounter()
+        {
+            Counter = 0;
+            CounterMamas = 0;
+            CounterEmpire = 0;
+            _lock = new object();
+        }
+        public void HandleThreads()
+        {
+            Thread EmpireThread = new Thread(Print1);
+            Thread MamasThread = new Thread(Print2);
+            EmpireThread.Start("Mamas");
+            MamasThread.Start("Empire");
+            EmpireThread.Join();
+            MamasThread.Join();
+            Console.WriteLine("This is " + CounterEmpire);
+            Console.WriteLine("This is " + CounterMamas);
+
+
+        }
+        public void Print1(object st)
+        {
+
+            for (int i = 0; i < 100; i = Counter)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(st);
+                    ++CounterMamas;
+                    Counter++;
+                }
+            }
+        }
+        public void Print2(object st)
+        {
+            for (int i = 0; i < 100; i = Counter)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(st);
+                    ++CounterEmpire;
+                    Counter++;
+
+                }
+                
+            }
+        }
+    }
+}
---
+++ HelloThread/HelloThread/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace HelloThread
+{
+    class Program
+    {
+        public static void Print(object st, object maxloop)
+        {
+            for(int i=0;i<(int)maxloop;i++)
+            { Console.WriteLine(st); }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            /*            Thread t1 = new Thread(() => Print("Empire", 1000));
+                        Thread t2 = new Thread(()=>Print("Mamas",1000));
+                        t2.Start();
+                        t1.Start();*/ //the first EX
+            PrintByCounter printByCounter = new PrintByCounter(); // second ex
+            printByCounter.HandleThreads();
+        }
+    }
+}
